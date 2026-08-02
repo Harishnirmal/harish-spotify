82fcb5e8fbc06d40f1cbeec1b8ff186429c35e39
Harishnirmal <harishnirmal.3@gmail.com>
2026-08-02 18:41:58 +0530

    first commit

---
+++ src/components/Opening.jsx
@@ -0,0 +1,12 @@
+const Opening = () => {
+  return (
+    <div className="h-screen w-full flex items-center justify-center bg-black text-white">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold">Spotify Clone</h1>
+        <p className="mt-4 text-slate-400">Loading...</p>
+      </div>
+    </div>
+  )
+}
+
+export default Opening
